c2f6b3725a7aa17c02ccb89cc34e62149f0ceac1
carlos-m-a <carlos.ma.work@gmail.com>
2016-11-15 18:24:53 +0100

    Actividad crear medicina iniciada

--- app/src/main/java/grupoasimov/pastillero/controladores/MainActivity.java
+++ app/src/main/java/grupoasimov/pastillero/controladores/MainActivity.java
@@ -1,15 +1,26 @@
 package grupoasimov.pastillero.controladores;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import grupoasimov.pastillero.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(this, CrearMedicina.class);
+        startActivity(i);
     }
 }
